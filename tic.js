52b403f04bb34fd85d9c8e9db6c46391dcba72c7
DevGoyal9238 <devgoyal9238@gmail.com>
2025-08-25 17:36:20 +0530

    Break

---
+++ tic.js
@@ -0,0 +1,17 @@
+const cell= document.querySelectorAll(".cell")
+const board= document.getElementById("board")
+const resetBtn=document.getElementById("resetBtn")
+
+const currentPlayer="X";
+
+const correctCombos=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
+
